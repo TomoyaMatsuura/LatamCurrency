34c19359dd99a705068420fbdc49403d56badfb4
niwa <naoki_niwa@mj.makita.co.jp>
2023-06-01 13:42:08 +0900

    mod

--- backend/main.go
+++ backend/main.go
@@ -8,12 +8,14 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly/v2"
 )
 
 type pageInfo struct {
 	Currency   string    `json:"currency"`
+	FY         string    `json:"FY"`
 	StatusCode int       `json:"statusCode"`
 	URL        string    `json:"url"`
 	Title      string    `json:"title"`
@@ -45,7 +47,7 @@ func savePageJson(fName string, p *pageInfo) {
 }
 
 // 小数点下2桁に丸める関数
-func roundToFourDecimalPlaces(num float64) float64 {
+func roundToTwoDecimalPlaces(num float64) float64 {
 	return math.Round(num*100) / 100
 }
 
@@ -54,18 +56,51 @@ func main() {
 
 	// 対象8通貨をループ
 	for i := 0; i < len(currency); i++ {
-		url := "https://finance.yahoo.com/quote/USD" + currency[i] + "%3DX/history?interval=1mo&filter=history&frequency=1mo"
+
+		// 今年の為替
+		year, month, _ := time.Now().Date()
+		startOfYear := time.Date(year, time.April, 1, 0, 0, 0, 0, time.UTC)
+		unixTime1 := startOfYear.Unix()
+		unixTime1Str := strconv.Itoa(int(unixTime1))
+
+		thisMonth := time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
+		unixTime2 := thisMonth.Unix()
+		unixTime2Str := strconv.Itoa(int(unixTime2))
+
+		url := "https://finance.yahoo.com/quote/USD" + currency[i] + "%3DX/history?period1=" + unixTime1Str + "&period2=" + unixTime2Str + "&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true"
+
+		// 前年度の為替
+		lastFiscalYearStart := time.Date(year-1, time.April, 1, 0, 0, 0, 0, time.UTC)
+		lastFiscalYearEnd := time.Date(year, time.March, 31, 0, 0, 0, 0, time.UTC)
+		unixTime1OfLastYear := lastFiscalYearStart.Unix()
+		unixTime1StrOfLastYear := strconv.Itoa(int(unixTime1OfLastYear))
+		unixTime2OfLastYear := lastFiscalYearEnd.Unix()
+		unixTime2StrOfLastYear := strconv.Itoa(int(unixTime2OfLastYear))
+
+		url2 := "https://finance.yahoo.com/quote/USD" + currency[i] + "%3DX/history?period1=" + unixTime1StrOfLastYear + "&period2=" + unixTime2StrOfLastYear + "&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true"
 
 		p := &pageInfo{
 			Currency: currency[i],
+			FY:       fmt.Sprintf("FY%d", year),
+		}
+
+		p2 := &pageInfo{
+			Currency: currency[i],
+			FY:       fmt.Sprintf("FY%d", year-1),
 		}
 
 		// コントローラを作成
 		c := colly.NewCollector()
+		c2 := colly.NewCollector()
 
 		// タイトルを取得
 		c.OnHTML("title", func(e *colly.HTMLElement) {
 			p.Title = e.Text
+			fmt.Println(e.Text)
+		})
+
+		c2.OnHTML("title", func(e *colly.HTMLElement) {
+			p2.Title = e.Text
 			fmt.Println(e.Text)
 		})
 
@@ -78,14 +113,33 @@ func main() {
 				month := strings.Split(date, " ")[0]
 				p.Month = append(p.Month, month)
 
-				// レートをfloat64として解析し、小数点以下4桁に丸める
+				// レートをfloat64として解析し、小数点以下2桁に丸める
 				r, err := strconv.ParseFloat(strings.ReplaceAll(rate, ",", ""), 64)
 				if err != nil {
 					log.Println("Error parsing rate:", err)
 					return
 				}
-				r = roundToFourDecimalPlaces(r)
+				r = roundToTwoDecimalPlaces(r)
 				p.Rate = append(p.Rate, r)
+			})
+		})
+
+		c2.OnHTML("table[data-test='historical-prices']", func(e *colly.HTMLElement) {
+			e.ForEach("tbody tr", func(_ int, el *colly.HTMLElement) {
+				date := el.ChildText("td:nth-child(1)")
+				rate := el.ChildText("td:nth-child(5)")
+
+				month := strings.Split(date, " ")[0]
+				p2.Month = append(p2.Month, month)
+
+				// レートをfloat64として解析し、小数点以下2桁に丸める
+				r, err := strconv.ParseFloat(strings.ReplaceAll(rate, ",", ""), 64)
+				if err != nil {
+					log.Println("Error parsing rate:", err)
+					return
+				}
+				r = roundToTwoDecimalPlaces(r)
+				p2.Rate = append(p2.Rate, r)
 			})
 		})
 
@@ -95,9 +149,19 @@ func main() {
 			fmt.Println("Visiting URL:", r.URL.String())
 		})
 
+		c2.OnRequest(func(r *colly.Request) {
+			p2.URL = r.URL.String()
+			fmt.Println("Visiting URL:", r.URL.String())
+		})
+
 		// Responseのステータスコードを取得
 		c.OnResponse(func(r *colly.Response) {
 			p.StatusCode = r.StatusCode
+			fmt.Println("StatusCode:", r.StatusCode)
+		})
+
+		c2.OnResponse(func(r *colly.Response) {
+			p2.StatusCode = r.StatusCode
 			fmt.Println("StatusCode:", r.StatusCode)
 		})
 
@@ -107,9 +171,18 @@ func main() {
 			log.Println("error:", r.StatusCode, err)
 		})
 
+		c2.OnError(func(r *colly.Response, err error) {
+			p2.StatusCode = r.StatusCode
+			log.Println("error:", r.StatusCode, err)
+		})
+
 		c.Visit(url)
+		c2.Visit(url2)
+
 		c.Wait()
+		c2.Wait()
 
 		savePageJson(fmt.Sprintf("rate_%s.json", currency[i]), p)
+		savePageJson(fmt.Sprintf("rate2_%s.json", currency[i]), p2)
 	}
 }
